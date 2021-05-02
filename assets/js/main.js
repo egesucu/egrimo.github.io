08c0fc16f36585710c788ccfeb03bb33f39e904b
Ege Sucu <ege@softhion.com>
2021-05-02 22:42:22 +0300

    initial commit V 1.0

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+    let accordion = document.getElementsByClassName("content-accordion");
+
+    let i;
+
+    for (i = 0; i < accordion.length; i++) {
+        accordion[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    }
